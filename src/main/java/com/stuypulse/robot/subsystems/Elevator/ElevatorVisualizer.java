ca8e8ceeb61edda4ccb2abf95ed5d21acc7e05b9
Brian Zheng <brianz76@nycstudents.net>
2025-01-13 17:01:32 -0500

    Optimize visualizer
    Co-authored-by: Zixi Qiao <Zixi52@users.noreply.github.com>
    Co-authored-by: Alvin Cheng <overseeroverseer@users.noreply.github.com>
    Co-authored-by: Ardian Agoes <ardianagoes@users.noreply.github.com>
    Co-authored-by:  Alan Liang <alanl100@users.noreply.github.com>
    Co-authored-by: Calvin Ye <cye2007@users.noreply.github.com>
    Co-authored-by: metereel <Material-Energy@users.noreply.github.com>
    Co-authored-by: sixuanl <sixuanl@users.noreply.github.com>
    Co-authored-by: Baiulus <Baiulus@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/subsystems/Elevator/ElevatorVisualizer.java
+++ src/main/java/com/stuypulse/robot/subsystems/Elevator/ElevatorVisualizer.java
@@ -4,71 +4,97 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 
 public class ElevatorVisualizer {
 
     private final Mechanism2d elevator2d;
     private final ElevatorImpl elevator;
 
+    private final MechanismRoot2d elevatorBL;
+    private final MechanismRoot2d elevatorTR;
+
+    private final MechanismRoot2d outerBL;
+    private final MechanismRoot2d outerTR;
+
+    private final MechanismRoot2d innerBL;
+    private final MechanismRoot2d  innerTR;
+
+
+
     public ElevatorVisualizer(ElevatorImpl elevator) {
+
+        // Mechanism 
         this.elevator = elevator;
-         
+        
         elevator2d = new Mechanism2d(6, 15);
         
         // Elevator Frame
-        elevator2d.getRoot("Base Origin", 0, 0).append(new MechanismLigament2d(
+
+        // bottom left node
+        elevatorBL = elevator2d.getRoot("Elevator BL", 0, 0);
+
+        elevatorBL.append(new MechanismLigament2d(
             "Left Tower",
             14, 
             0,
             10, 
-            new Color8Bit(Color.kOrange)));
+            new Color8Bit(Color.kOrange)
+        ));
 
-        elevator2d.getRoot("Base Origin", 0, 0).append(new MechanismLigament2d(
+        elevatorBL.append(new MechanismLigament2d(
             "Bottom Tower",
             6, 
             -90,
             10, 
-            new Color8Bit(Color.kOrange)));
+            new Color8Bit(Color.kOrange)
+        ));
 
-        elevator2d.getRoot("Base Origin", 6, 0).append(new MechanismLigament2d(
+        // top right node
+        elevatorTR = elevator2d.getRoot("Elevator TR", 6, 0);
+
+        elevatorTR.append(new MechanismLigament2d(
             "Right Tower", 
             14,
-            0,
+            180,
             10,
             new Color8Bit(Color.kOrange)
         ));
 
-        elevator2d.getRoot("Base Origin", 0, 14).append(new MechanismLigament2d(
+        elevatorTR.append(new MechanismLigament2d(
             "Top Side",
             6,
-            -90, 
+            90, 
             10, 
             new Color8Bit(Color.kOrange)));
 
-
         //outerFrame
-        elevator2d.getRoot("Base Origin", 1, 1).append(new MechanismLigament2d(
+
+        // bottom left node
+        outerBL = new 
+        elevator2d.getRoot("Outer Bottom Left", 1, 1).append(new MechanismLigament2d(
             "Left Side",
             14,
             0, 
             10,
             new Color8Bit(Color.kYellow)));
     
-        elevator2d.getRoot("Base Origin", 1, 1).append(new MechanismLigament2d(
+        elevator2d.getRoot("Outer Bottom Left", 1, 1).append(new MechanismLigament2d(
             "Bottom Side",
             4,
             -90,
             10,
             new Color8Bit(Color.kYellow)));
-            
-        elevator2d.getRoot("Base Origin", 1, 15).append(new MechanismLigament2d(
+        
+        // top right node
+        elevator2d.getRoot("Outer Top Right", 1, 15).append(new MechanismLigament2d(
             "Top Side",
             4,
             -90,
             10,
             new Color8Bit(Color.kYellow)));
 
-        elevator2d.getRoot("Base Origin", 5, 1).append(new MechanismLigament2d(
+        elevator2d.getRoot("Ba", 5, 1).append(new MechanismLigament2d(
             "Right Side",
             14,
             0,
@@ -77,6 +103,8 @@ public class ElevatorVisualizer {
         
 
         //innerFrame
+
+        // bottom left node
         elevator2d.getRoot("Base Origin", 2, 2).append(new MechanismLigament2d(
             "Bottom Side",
             2,
@@ -90,7 +118,8 @@ public class ElevatorVisualizer {
             0, 
             10,
             new Color8Bit(Color.kPink)));
-            
+        
+        // top right node
         elevator2d.getRoot("Base Origin", 2, 4).append(new MechanismLigament2d(
             "Top Side",
             2,
@@ -107,6 +136,6 @@ public class ElevatorVisualizer {
     }
 
     public void update() {
-
+        elevator2d.getRoot("Outer Bottom Left")
     }
 }
