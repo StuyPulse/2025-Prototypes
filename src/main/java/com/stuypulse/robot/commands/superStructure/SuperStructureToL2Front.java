1ea1d98913df03e9f2a2b79790af7986e8babe45
WennieC <chenweifen913@gmail.com>
2025-05-12 16:15:30 -0400

    more commands

---
+++ src/main/java/com/stuypulse/robot/commands/superStructure/SuperStructureToL2Front.java
@@ -0,0 +1,10 @@
+package com.stuypulse.robot.commands.superStructure;
+
+import com.stuypulse.robot.subsystems.superStructure.SuperStructure.SuperStructureState;
+
+public class SuperStructureToL2Front extends SuperStructureSetState {
+
+    public SuperStructureToL2Front() {
+        super(SuperStructureState.L2_FRONT);
+    }
+}
---
+++ src/main/java/com/stuypulse/robot/commands/superStructure/SuperStructureToL3Front.java
@@ -0,0 +1,13 @@
+package com.stuypulse.robot.commands.superStructure;
+
+import com.stuypulse.robot.subsystems.superStructure.SuperStructure.SuperStructureState;
+
+public class SuperStructureToL3Front extends SuperStructureSetState {
+
+    public SuperStructureToL3Front() {
+        super(SuperStructureState.L3_FRONT);
+    }
+
+}
+    
+
