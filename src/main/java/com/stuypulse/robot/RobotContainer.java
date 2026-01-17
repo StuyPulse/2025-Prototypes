0944ffdec9d1aa9f55fef525904efaff535e2c60
mohammadr09 <mohammadnyc96@gmail.com>
2026-01-17 12:04:50 -0500

    Shooter Keybinds Configuration
    Co-authored-by: Alwin <AlwinNg@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -9,6 +9,9 @@ import com.stuypulse.robot.commands.auton.DoNothingAuton;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
+import com.stuypulse.robot.subsystems.shooter.Shooter;
+import com.stuypulse.robot.commands.shooter.ShooterStop;
+import com.stuypulse.robot.commands.shooter.ShooterShoot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -21,6 +24,7 @@ public class RobotContainer {
     public final Gamepad operator = new AutoGamepad(Ports.Gamepad.OPERATOR);
     
     // Subsystem
+    private final Shooter shooter = Shooter.getInstance();
 
     // Autons
     private static SendableChooser<Command> autonChooser = new SendableChooser<>();
@@ -37,13 +41,20 @@ public class RobotContainer {
     /*** DEFAULTS ***/
     /****************/
 
-    private void configureDefaultCommands() {}
+    private void configureDefaultCommands() {
+        shooter.setDefaultCommand(new ShooterStop());
+    }
 
     /***************/
     /*** BUTTONS ***/
     /***************/
 
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        // shooter button bindings
+        driver.getRightBumper()
+            .whileTrue(new ShooterShoot())
+            .whileFalse(new ShooterStop());
+    }
 
     /**************/
     /*** AUTONS ***/
